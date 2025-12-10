9fbf2111cba27ea623f3f86e46d0e8edbb8cebbd
abhi-thecoder <abhishekkumarmahto2005@gmail.com>
2025-12-11 02:06:50 +0530

    Added backend_urls

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,3 +1,5 @@
-BACKEND_URL = "http://localhost:5001"
-const SOCKET_IO_URL = 'http://localhost:5001';
+BACKEND_URL = "https://backend-quick-pick-redesigned.onrender.com"
+SOCKET_IO_URL = "https://backend-quick-pick-redesigned.onrender.com"
+// BACKEND_URL = "http://localhost:5001"
+// SOCKET_IO_URL = 'http://localhost:5001';
 // BACKEND_URL = 
